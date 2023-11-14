65836766d1b39cc73b801e78a721fb8fc37a77e6
Paresh Adhia <padhia@gmail.com>
2023-11-14 15:13:28 -0500

    update snowpark to 1.10.0

--- snowpark.nix
+++ snowpark.nix
@@ -10,12 +10,12 @@
 
 buildPythonPackage rec {
   pname = "snowflake-snowpark-python";
-  version = "1.9.0";
+  version = "1.10.0";
   format = "setuptools";
 
   src = fetchPypi {
     inherit pname version;
-    hash = "sha256-elp9kBtiicMgOmYgnKwwRuVhGR/6AEjz5kJyRZUHydo=";
+    hash = "sha256-epu8R+rGhit7zRcs7I5nhupsdbPcJ6I2a5MPsOzLDKU=";
   };
 
   propagatedBuildInputs = [
